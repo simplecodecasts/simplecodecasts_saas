7a9eaacdecc9e50df35a7465f2957c0d3ff60a7d
Rob Dev <rob@simplecodecasts.com>
2014-12-22 01:18:48 +0000

    Added contacts route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.

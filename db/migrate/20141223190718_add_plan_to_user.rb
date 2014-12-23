c7be314236aec276ad38470b1c44fd9af7ec3a79
Rob Dev <rob@simplecodecasts.com>
2014-12-23 19:10:43 +0000

    Added association for plans and users

---
+++ db/migrate/20141223190718_add_plan_to_user.rb
@@ -0,0 +1,5 @@
+class AddPlanToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :plan_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223183721) do
+ActiveRecord::Schema.define(version: 20141223190718) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20141223183721) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

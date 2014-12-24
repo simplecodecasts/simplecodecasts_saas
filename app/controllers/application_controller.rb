e2de6d87f6d131b2634aeac63fa3f7a1670c380e
Rob Dev <rob@simplecodecasts.com>
2014-12-24 01:01:45 +0000

    Fixed typos in Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   
   protected
-    def configure_permitted_paramaters
-      devise_paramater_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) }
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) }
     end
 end

442023e04b9761abcb9e80dd3e41bf298516b411
Rob Dev <rob@simplecodecasts.com>
2014-12-24 18:35:12 +0000

    Restarting from profiles controller video

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,22 +1,3 @@
 class ProfilesController < ApplicationController
-  def new
-    @user = User.find(params[:user_id])
-    @profile = @user.build_profile
-  end
-  
-  def create
-    @user = User.find(params[:user_id])
-    @profile = @user.build_profile(profile_params)
-    if @profile.save
-      flash[:success] = "Profile updated!"
-      redirect_to user_path(params[:user_id])
-    else
-      render action: :new
-    end
-  end
-  
-  private
-    def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-    end
+
 end

2c959f6460af01b4be08d33bbd3704dd7acda9f8
Rob Dev <rob@simplecodecasts.com>
2014-12-26 05:07:48 +0000

    Added file fields for image upload on profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -37,7 +37,7 @@ class ProfilesController < ApplicationController
   
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
   
     def only_current_user

1be98a4886b9a560cb1fab844058b51a0dac514c
Rob Dev <rob@simplecodecasts.com>
2014-12-26 04:11:03 +0000

    Added form partial, and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,6 +16,11 @@ class ProfilesController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
